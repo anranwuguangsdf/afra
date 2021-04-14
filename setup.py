19a1a8c68b86e925de764f2a851774868f084e5d
Jiaxin Wang <jiaxin.wang@sjtu.edu.cn>
2021-04-14 16:15:10 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name="afra",
-      version="1.1.1",
+      version="1.2.0",
       description="AliCPT Foreground Removal Analysis",
+      author="Jiaxin Wang, Jian Yao, Le Zhang", 
       license="GPLv3",
-      url="https://github.com/gioacchinowang/afra",
+      url="https://github.com/afra-dev/afra",
       packages=find_packages(),
       dependency_links=[],
       python_requires='>=3.5',
-      install_requires=['numpy', 'scipy', 'healpy'],
+      install_requires=['numpy', 'scipy', 'healpy', 'pymaster', 'dynesty', 'emcee'],
       zip_safe=False,
       classifiers=["Development Status :: 4 - Beta",
                    "Topic :: Utilities",
